b2c971fce98b4262bda8ba039fff48f6861e4f8e
Diego Sedlaruk <diego.sedlaruk@live.com.ar>
2018-12-22 13:54:17 -0300

    first commit

---
+++ src/main/java/com/eduit/App.java
@@ -0,0 +1,13 @@
+package com.eduit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
